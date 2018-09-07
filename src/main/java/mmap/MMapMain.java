34e77b0838d316f1cd96e065b8553b4310856c28
longhe <LONGHE001@huazhu.com>
2018-09-07 14:08:35 +0800

    增加页面

---
+++ src/main/java/mmap/MMapMain.java
@@ -0,0 +1,10 @@
+package mmap;
+
+/**
+ * mmap模式入口
+ */
+public class MMapMain {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ src/main/java/nio/NioMain.java
@@ -0,0 +1,10 @@
+package nio;
+
+/**
+ * NIO文件操作的入口
+ */
+public class NioMain {
+    public static void main(String args[]){
+
+    }
+}
